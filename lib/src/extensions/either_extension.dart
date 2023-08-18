d4f079cac99a8c16eb91681a2b238ab666ac80ec
yeasin50 <yeasinsheikh50@gmail.com>
2023-08-19 02:04:37 +0600

    fixed-typo

--- lib/src/extensions/either_extension.dart
+++ lib/src/extensions/either_extension.dart
@@ -1,6 +1,6 @@
-import 'package:dartz/dartz.dart';
+part of my_utils;
 
 extension EitherX<L, R> on Either<L, R> {
-  R asRight() => (this as Right).value;  
+  R asRight() => (this as Right).value;
   L asLeft() => (this as Left).value;
 }
