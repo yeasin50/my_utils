e3d06a2cd6da36ce01d1d87955e7c47113badf43
yeasin50 <yeasinsheikh50@gmail.com>
2023-12-12 18:53:29 +0600

    documented

--- lib/src/usecases/usecase.dart
+++ lib/src/usecases/usecase.dart
@@ -1,5 +1,9 @@
 part of my_utils;
 
+/// A `UseCase` is a class that represents a single use case of your app.
+/// It is the entry point to the domain layer.
 abstract class UseCase<Type, Params> {
+
+  /// Takes a [Params] object and executes the use case.
   Future<Either<Failure, Type>> call(Params params);
 }
